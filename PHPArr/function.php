763e741340f534ef9debde8f879b1c7b0a3d15df
Gail <xxxggggail@gmail.com>
2019-10-08 23:49:02 +0800

    :pencil2:

--- PHPArr/function.php
+++ PHPArr/function.php
@@ -465,9 +465,41 @@ function flipAndInvertImage($A) {
     return $A;
 }
 
-$A = [
-    [0,0,1],
-    [1,1,0]
-];
-$data = flipAndInvertImage($A);
-var_dump($data);
+//$A = [
+//    [0,0,1],
+//    [1,1,0]
+//];
+//$data = flipAndInvertImage($A);
+//var_dump($data);
+
+/**
+ * @Time: 2019/10/8 21:11
+ * @DESC: 5079.简单
+ * 给出三个均为 严格递增排列 的整数数组 arr1，arr2 和 arr3。
+ * 返回一个由 仅 在这三个数组中 同时出现 的整数所构成的有序数组。
+ * @param $arr1
+ * @param $arr2
+ * @param $arr3
+ * @return array
+ */
+function arraysIntersection($arr1, $arr2, $arr3) {
+    $array = array_intersect($arr1,$arr2,$arr3); // (ー△ー；) 感觉这个解法有点太简单了emm
+    return $array;
+}
+
+function uniqueMorseRepresentations($words) {
+    $dict = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."];
+    $new_data = [];
+    foreach ($words as $word) {
+        $word = str_split($word);
+        if($word){
+            $new_word = '';
+            foreach ($word as $item) {
+                $new_word .= $dict[ord($item)-97];
+            }
+            $new_data[] = $new_word;
+        }
+    }
+    $new_data = array_unique($new_data);
+    return count($new_data);
+}
