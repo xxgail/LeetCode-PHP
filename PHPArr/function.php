cc8eb48b58a6cbbbff4b2d626ecbeb9ebb53aad4
Gail <xxxggggail@gmail.com>
2019-10-15 16:50:43 +0800

    :pencil2: 10.15

--- PHPArr/function.php
+++ PHPArr/function.php
@@ -637,7 +637,26 @@ function subsetsFunc($nums,$index,&$current,&$res)
     }
 }
 
+/**
+ * @Time: 2019/10/15 11:48
+ * @DESC: 260.
+ * 给定一个整数数组 nums，其中恰好有两个元素只出现一次，其余所有元素均出现两次。 找出只出现一次的那两个元素。
+ * @param $nums
+ * @return array
+ */
+function singleNumberTwo($nums) {
+    $data = [];
+    $nums = array_count_values($nums);
+    foreach ($nums as $k => $num) {
+        if($num == 1){
+            $data[] = $k;
+        }
+    }
+    return $data;
+}
 
 
-$da = subsets([3,2,1]);
+$a = "1+1i";
+$b = "1+1i";
+$da = complexNumberMultiply($a,$b);
 var_dump($da);
--- PHPTreeNode/function.php
+++ PHPTreeNode/function.php
@@ -357,5 +357,45 @@ function pruneTree($root) {
     return $root;
 }
 
-$data = pruneTree($root);
+
+/**
+ * @Time: 2019/10/15 16:34
+ * @DESC: 230. 二叉搜索树中第K小的元素
+ * 给定一个二叉搜索树，编写一个函数 kthSmallest 来查找其中第 k 个最小的元素。
+ * 说明：
+ * 你可以假设 k 总是有效的，1 ≤ k ≤ 二叉搜索树元素个数。
+ * @param $root
+ * @param $k
+ * @return mixed
+ */
+function kthSmallest($root, $k) {
+    $result = [];
+    inorderTraversalFunc($root,$result);
+    return $result[$k - 1];
+}
+
+/**
+ * @Time: 2019/10/15 16:33
+ * @DESC: 用递归方法中序遍历二叉树返回数组
+ * @param $root
+ * @param $result
+ */
+function inorderTraversalFunc($root,&$result){
+    if($root->left != null){
+        inorderTraversalFunc($root->left,$result);
+    }
+
+    $result[] = $root->val;
+
+    if($root->right != null){
+        inorderTraversalFunc($root->right,$result);
+    }
+}
+
+$root = new TreeNode(7);
+$root->left = new TreeNode(3);
+$root->left->left = new TreeNode(1);
+$root->left->right = new TreeNode(4);
+$root->right = new TreeNode(9);
+$data = kthSmallest($root,2);
 var_dump($data);
