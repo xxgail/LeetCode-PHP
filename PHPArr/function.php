2f68d51c506fb174bca6b21f2f85735868b6538b
Gail <xxxggggail@gmail.com>
2019-10-17 17:50:03 +0800

    :pencil2: 10.17

--- PHPArr/function.php
+++ PHPArr/function.php
@@ -655,16 +655,23 @@ function singleNumberTwo($nums) {
     return $data;
 }
 
+/**
+ * @Time: 2019/10/17 11:40
+ * @DESC: 287. 寻找重复数
+ * 给定一个包含 n + 1 个整数的数组 nums，其数字都在 1 到 n 之间（包括 1 和 n），可知至少存在一个重复的整数。假设只有一个重复的整数，找出这个重复的数。
+ * @param $nums
+ * @return mixed
+ */
 function findDuplicate($nums) {
-    for ($i = 0; $i < count($nums); $i++){
-        if(array_search($nums[$i],$nums) != $i){
+    // 不能改变原数组的值
+    sort($nums);
+    for ($i = 0; $i < count($nums) - 1; $i++){
+        if($nums[$i] == $nums[$i+1]){
             return $nums[$i];
         }
     }
 }
 
 
-$a = "1+1i";
-$b = "1+1i";
-$da = complexNumberMultiply($a,$b);
+$da = findDuplicate([1,2,3,4,3]);
 var_dump($da);
