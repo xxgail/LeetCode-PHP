bd5acbccf69fb5119954567da577a5e5de21b6bd
xxgail <xxxggggail@gmail.com>
2020-03-05 21:23:52 +0800

    :pencil: 03.05

---
+++ GreedyAlgorithm/m-402-removeKdigits.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @Time: 2020-02-20 21:23
+ * @DESC: 402. 移掉K位数字 todo:
+ * 给定一个以字符串表示的非负整数 num，移除这个数中的 k 位数字，使得剩下的数字最小。
+ * 注意:
+ * num 的长度小于 10002 且 ≥ k。
+ * num 不会包含任何前导零。
+ * @param $num
+ * @param $k
+ * @return int
+ */
+function removeKdigits($num,$k){
+   return $num;
+}
+
+var_dump(removeKdigits(1432219,3));
--- Stack/m-1190-reverseParentheses.php
+++ Stack/m-1190-reverseParentheses.php
@@ -1,4 +1,19 @@
 <?php
+/**
+ * @Time: 2020-02-20 14:23
+ * @DESC: 1190. 反转每对括号间的子串 中等
+ * 给出一个字符串 s（仅含有小写英文字母和括号）。
+ * 请你按照从括号内到外的顺序，逐层反转每对匹配括号中的字符串，并返回最终的结果。
+ * 注意，您的结果中 不应 包含任何括号。
+ * 示例 1：
+ * 输入：s = "(abcd)"
+ * 输出："dcba"
+ * 示例 2：
+ * 输入：s = "(u(love)i)"
+ * 输出："iloveu"
+ * @param $s
+ * @return mixed
+ */
 function reverseParentheses($s){
     $strings = [''];
     $len = strlen($s);
@@ -27,3 +42,11 @@ $s3 = "(u(love)i)";
 print_r(reverseParentheses($s3));
 $s4 = "kae(c(x((ffr()(t(ky)esr()oc)bl)kwx)qz)a(yovg(())dpx((w(co(tuz))zldu(nukcno(j)nohpg)g)bxp)djjbw))ho(h)";
 print_r(reverseParentheses($s4) == "kaeyovgdpxpxbwtuzoczldugphonjonckungdjjbwaxxwkffrcorsekytblqzchoh");
+
+###
+# 还是用栈的思想，开始的时候想着用一个普通的栈，把正括号存进去，在遇到反括号的时候再将该正括号删除。
+# 但是可能是条件写的不够准确，还是会有几个案例过不去。
+# 比如遇到单独的()或者(a)的时候，应该将它加到上一个存进栈中的字符串的后面。
+# 这个就比较方便了，虽然还是参考别人的想法，所以一定要记住这个解题思路。
+# 解题思路是：遇到正括号时存入一个新的空字符串，将每个字符都加到栈的最后一个元素的末尾。在遇到反括号时，将最后一个元素反转，加到前一个元素的末尾。
+# 就很nice(๑•̀ㅂ•́)و✧
