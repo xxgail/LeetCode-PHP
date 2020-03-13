539b9e586749bc8cdf2cc691ad78a2f8ccbe857e
xxgail <xxxggggail@gmail.com>
2020-03-13 23:37:40 +0800

    :rocket: 03.13 消灭TODO

--- SlidingWindow/e-mst57-findContinuousSequence.php
+++ SlidingWindow/e-mst57-findContinuousSequence.php
@@ -8,29 +8,61 @@
  * @return array
  */
 function findContinuousSequence($target){
+    # ----- 普通解法 184ms
     $result = [];
-    $i = 1;
-    while ($i < $target/2){
-        $j = $i;
-        $sum = $target;
-        $item = [];
-        while ($sum >= $j){
-            $item[] = $j;
-            $sum -= $j;
-            $j ++;
+//    $i = 1;
+//    while ($i < $target/2){
+//        $j = $i;
+//        $sum = $target;
+//        $item = [];
+//        while ($sum >= $j){
+//            $item[] = $j;
+//            $sum -= $j;
+//            $j ++;
+//        }
+//        if ($sum == 0){
+//            $result[] = $item;
+//        }
+//        $i ++;
+//    }
+
+    # ----- 双指针OK 68ms
+    $left = 1;
+    while ($left <= $target/2){
+        $sum = $right = $left;
+        while ($sum < $target){
+            $right ++;
+            $sum += $right;
         }
-        if ($sum == 0){
-            $result[] = $item;
+        if ($sum == $target){
+            $result[] = range($left,$right);
         }
-        $i ++;
+        $left++;
     }
 
+    # ----- 滑动窗口 60ms
+//    $left = 1;
+//    $right = 2;
+//    while ($left < $right && $left <= $target/2){
+//        $sum = ($left + $right) * ($right - $left + 1) / 2;
+//        if ($sum == $target){
+//            $result[] = range($left,$right);
+//            $left ++;
+//            $right ++;
+//        }elseif ($sum < $target){
+//            $right ++;
+//        }else{
+//            $left ++;
+//        }
+//    }
     return $result;
 }
 
-print_r(findContinuousSequence(100000));
+print_r(findContinuousSequence(15));
 
 ###
 # 暴力循环法解决了，看到题解才知道是滑动窗口的题（应该算是吧
-# 所以还需要用滑动窗口写一遍 TODO
+# 所以还需要用滑动窗口写一遍
 
+# range($start,$end,[$step=1]) 根据范围创建数组，包含指定的元素
+# range(1,3) = [1,2,3]
