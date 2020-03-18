d76b217ecac079afd2ad494686affb4f8f3085dd
Gail <xxxggggail@gmail.com>
2020-03-18 17:42:23 +0800

    :seedling:

--- DynamicProgramming/m-279-numSquares.php
+++ DynamicProgramming/m-279-numSquares.php
@@ -16,7 +16,7 @@ function numSquares($n) {
     $dp[0] = 0;
     $dp[1] = 1;
     for ($i = 2; $i <= $n; $i++) {
-        $min = $n;
+        $min = $n; // 初始化为n，就是全部由1组成
         for ($j = 1; $j * $j <= $i; $j++) {
             if ($j * $j == $i) {
                 $min = 1;
@@ -28,13 +28,16 @@ function numSquares($n) {
     }
 
     return $dp[$n];
+}
 
-    # 超出时间限制 ╥﹏╥
+##----
+# 动态规划。
+
+
+// 超出时间限制 ╥﹏╥
 //    $min = $n;
 //    numSquaresFunc($n,0,$min);
 //    return $min;
-}
-
 function numSquaresFunc($n,$res,&$min){
     if($n == 0){
         if($res < $min){
@@ -55,6 +58,29 @@ function numSquaresFunc($n,$res,&$min){
     }
 }
 
-##----
-# 动态规划。
-# 可能我想的方向还是不对。
+// 数学的解法，评论区的大佬真的是什么都会
+function numSquaresMath($n){
+    while ($n % 4 === 0) {
+        $n /= 4;
+    }
+    if ($n % 8 === 7) { // 参考28（25+1+1+1），除以4 = 7（1+4+1+1）
+        return 4;
+    }
+
+    $a = 0;
+    while ($a * $a <= $n) { // 注意条件
+        $b = (int)sqrt($n - $a * $a);
+        if ($a*$a + $b*$b === $n) {
+            return !!$a + !!$b; // 其实就是返回2或者1
+//            return ($a?1:0) + ($b?1:0); // 相当于这个
+        }
+        $a++;
+    }
+
+    return 3;
+}
+
+var_dump(numSquaresMath(25));
+
+### tips
+# !!$a 双感叹号代表 !(!$a)，表示真实存在的数据转换成布尔值。就是如果a>0就返回1，=0就返回0。
